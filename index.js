3b75ae9d2c88c0905fa0e78ff30dce85b85e5dc7
Esin <nbusra.esin@gmail.com>
2023-03-31 17:27:47 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+
+let random = parseInt(Math.random() +1)
+
+let num3 = 5
+let num4 = 9
+let mod = num4%num3 
+
+
+let max = Math.max(1,2,3,20)
